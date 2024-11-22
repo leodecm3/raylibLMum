4196b05748a6f63182d9530e85f058516ba5d75d
Leonardo Mezavilla <30425975+leodecm3@users.noreply.github.com>
2024-11-21 21:38:48 -0500

    init

---
+++ projects/scripts/build.bat
@@ -0,0 +1,26 @@
+::@echo off
+:: > Setup required Environment
+:: -------------------------------------
+set COMPILER_DIR=C:\raylib\w64devkit\bin
+set PATH=%PATH%;%COMPILER_DIR%
+cd %~dp0
+:: .
+:: > Compile simple .rc file
+:: ----------------------------
+cmd /c windres ..\..\src\rayliblmum.rc -o ..\..\src\rayliblmum.rc.data
+:: .
+:: > Generating project
+:: --------------------------
+cmd /c mingw32-make -f ..\..\src\Makefile ^
+PROJECT_NAME=rayliblmum ^
+PROJECT_VERSION=1.0 ^
+PROJECT_DESCRIPTION="my cool new project" ^
+PROJECT_INTERNAL_NAME=rayliblmum ^
+PROJECT_PLATFORM=PLATFORM_DESKTOP ^
+PROJECT_SOURCE_FILES="rayliblmum.c" ^
+BUILD_MODE="RELEASE" ^
+BUILD_WEB_ASYNCIFY=FALSE ^
+BUILD_WEB_MIN_SHELL=TRUE ^
+BUILD_WEB_HEAP_SIZE=268435456 ^
+RAYLIB_MODULE_AUDIO=TRUE ^
+RAYLIB_MODULE_MODELS=TRUE
